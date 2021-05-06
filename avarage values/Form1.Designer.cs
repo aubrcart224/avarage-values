b17ae58c795808777106ada2bb3f195a45a22596
aubrcart224 <aubrcart224@ed.amdsb.ca>
2021-05-06 09:58:29 -0400

    Add project files.

---
+++ avarage values/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace avarage_values
+{
+    partial class averageNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.titalLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(404, 89);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(284, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // titalLabel
+            // 
+            this.titalLabel.BackColor = System.Drawing.Color.MistyRose;
+            this.titalLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titalLabel.Location = new System.Drawing.Point(2, 20);
+            this.titalLabel.Name = "titalLabel";
+            this.titalLabel.Size = new System.Drawing.Size(798, 28);
+            this.titalLabel.TabIndex = 2;
+            this.titalLabel.Text = "Enter a number and press add";
+            this.titalLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(248, 140);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(198, 23);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(284, 243);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(195, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset ";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(365, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter 0 and press add to calculate the average of the sum";
+            // 
+            // averageNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.titalLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addButton);
+            this.Name = "averageNumbers";
+            this.Text = "Average Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label titalLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ avarage values/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace avarage_values
+{
+    public partial class averageNumbers : Form
+    {
+        //global variables
+        double sum;
+        double input;
+        double numberOfNumbers;
+        double average = 0;
+
+        public averageNumbers()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //variables 
+
+            input = Convert.ToDouble(textBox1.Text);
+            
+
+            //calcualtions 
+
+            if (input  == 0)
+            {
+                average = sum / numberOfNumbers;
+                outputLabel.Text = $"The Average is {average} "; 
+            }
+            else 
+            {
+                numberOfNumbers++;
+                sum = sum + input;
+                outputLabel.Text = $" {input} was added to the sum";
+            
+            }
+
+            
+
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            input = 0;
+            numberOfNumbers = 0;
+            average = 0;
+        }
+    }
+}
